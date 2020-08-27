1907db00d63ceb3fc86d2d7d3b0cec77e61824bd
BedoyaAlban <a.bedoya@outlook.fr>
2020-08-27 18:12:08 +0200

    Show all the months + display dynamically the current month

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,7 +5,9 @@ const Calendar = () => {
   const [date, setDate] = useState({
     dateData: moment()
   });
+
   const weekdays = moment.weekdays();
+  // const allMonths = moment.months();
   // Return one TH by day
   let weekdaysShortName = weekdays.map(day => {
     return (
@@ -36,16 +38,28 @@ const Calendar = () => {
   const DaysInMonth = () => {
     return date.dateData.daysInMonth();
   };
+  // String of the current day
+  const cDay = () => {
+    return date.dateData.format("D");
+  };
+  // String of the current month
+  const cMonth = () => {
+    return date.dateData.format("MMMM");
+  };
+
+  console.log(cMonth());
   // Return a TD for each day in the month
   let daysInMonth = [];
   for (let d = 1; d <= DaysInMonth(); d++) {
+    let currentDay = d == cDay() ? "day today" : "day";
+
     daysInMonth.push(
-      <td key={d} className="calendar-day">
+      <td key={d} className={`${currentDay}`}>
         {d}
       </td>
     );
   }
-
+  //
   var totalSlots = [...blanks, ...daysInMonth];
   let rows = [];
   let cells = [];
@@ -67,10 +81,57 @@ const Calendar = () => {
   let daysinmonth = rows.map((d, i) => {
     return <tr key={i}>{d}</tr>;
   });
+  // Function to return a table of all the month in one year
+  const ListOfMonth = props => {
+    let months = [];
+    props.data.map(data => {
+      months.push(
+        <td key={data}>
+          <span>{data}</span>
+        </td>
+      );
+    });
+
+    let rows = [];
+    let cells = [];
+
+    months.forEach((row, i) => {
+      if (i % 3 !== 0 || i == 0) {
+        // except zero index
+        cells.push(row);
+      } else {
+        rows.push(cells);
+        cells = [];
+        cells.push(row);
+      }
+    });
+    rows.push(cells); // add last row
+
+    let monthlist = rows.map((d, i) => {
+      return <tr key={i}>{d}</tr>;
+    });
+
+    return (
+      <table className="months">
+        <thead>
+          <tr className="header-months">
+            <th colSpan="3">Select a Month</th>
+          </tr>
+        </thead>
+        <tbody>{monthlist}</tbody>
+      </table>
+    );
+  };
 
   return (
-    <div>
+    <div className="container">
       <h2>Calendar</h2>
+      <div>
+        <span>{cMonth()}</span>
+      </div>
+      <div>
+        <ListOfMonth data={moment.months()} />
+      </div>
       <table className="calendar">
         <thead>
           <tr>{weekdaysShortName}</tr>
